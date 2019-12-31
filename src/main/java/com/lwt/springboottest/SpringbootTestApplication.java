0fd945e8c74e4679f35b8a09819ffc037ef1aca0
lwt <874612642@qq.com>
2019-12-31 14:45:16 +0800

    23

--- src/main/java/com/lwt/springboottest/SpringbootTestApplication.java
+++ src/main/java/com/lwt/springboottest/SpringbootTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootTestApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringbootTestApplication.class, args);
     }
 
